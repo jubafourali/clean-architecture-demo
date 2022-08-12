09bb55dbe943060083c0cd0df05e4c956affd110
jubafourali <jubafourali@gmail.com>
2022-08-12 09:33:32 +0400

    Refactoer code

--- src/main/java/com/cleancode/cleanarchitecturedemo/infrastructure/controller/UserController.java
+++ src/main/java/com/cleancode/cleanarchitecturedemo/infrastructure/controller/UserController.java
@@ -6,7 +6,6 @@ import com.cleancode.cleanarchitecturedemo.usecase.command.CreateUser;
 import com.cleancode.cleanarchitecturedemo.usecase.input.UserInput;
 import com.cleancode.cleanarchitecturedemo.usecase.query.GetAllUsers;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
