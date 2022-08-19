df53515e118adeaa257ab16d973abdc768cebffc
jubafourali <jubafourali@gmail.com>
2022-08-19 09:59:17 +0400

    Refactor code

--- src/test/java/com/cleancode/cleanarchitecturedemo/infrastructure/storage/mongodb/MongodbUserStorageTest.java
+++ src/test/java/com/cleancode/cleanarchitecturedemo/infrastructure/storage/mongodb/MongodbUserStorageTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 import java.util.UUID;
